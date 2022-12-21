ad10aa50037186b0a1dba1183fa4c9dd90ce9c95
Mohammad Reza Yadollahi <80624792+Phoenix15049@users.noreply.github.com>
2022-12-22 00:51:57 +0330

    first commit

---
+++ Mainapp.py
@@ -0,0 +1,117 @@
+
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'MainappGHreEB.ui'
+##
+## Created by: Qt User Interface Compiler version 5.14.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import (QCoreApplication, QMetaObject, QObject, QPoint,
+    QRect, QSize, QUrl, Qt)
+from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont,
+    QFontDatabase, QIcon, QLinearGradient, QPalette, QPainter, QPixmap,
+    QRadialGradient)
+from PySide2.QtWidgets import *
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.setWindowModality(Qt.NonModal)
+        MainWindow.setEnabled(True)
+        MainWindow.resize(760, 593)
+        palette = QPalette()
+        brush = QBrush(QColor(0, 0, 0, 255))
+        brush.setStyle(Qt.SolidPattern)
+        palette.setBrush(QPalette.Active, QPalette.WindowText, brush)
+        brush1 = QBrush(QColor(109, 103, 228, 255))
+        brush1.setStyle(Qt.SolidPattern)
+        palette.setBrush(QPalette.Active, QPalette.Button, brush1)
+        brush2 = QBrush(QColor(242, 247, 161, 255))
+        brush2.setStyle(Qt.SolidPattern)
+        palette.setBrush(QPalette.Active, QPalette.Light, brush2)
+        palette.setBrush(QPalette.Active, QPalette.Midlight, brush1)
+        palette.setBrush(QPalette.Active, QPalette.Dark, brush1)
+        palette.setBrush(QPalette.Active, QPalette.Mid, brush1)
+        palette.setBrush(QPalette.Active, QPalette.Text, brush)
+        palette.setBrush(QPalette.Active, QPalette.ButtonText, brush)
+        brush3 = QBrush(QColor(69, 60, 103, 255))
+        brush3.setStyle(Qt.SolidPattern)
+        palette.setBrush(QPalette.Active, QPalette.Base, brush3)
+        palette.setBrush(QPalette.Active, QPalette.Window, brush3)
+        brush4 = QBrush(QColor(70, 194, 203, 255))
+        brush4.setStyle(Qt.SolidPattern)
+        palette.setBrush(QPalette.Active, QPalette.Highlight, brush4)
+        palette.setBrush(QPalette.Active, QPalette.ToolTipBase, brush2)
+        brush5 = QBrush(QColor(0, 0, 0, 128))
+        brush5.setStyle(Qt.SolidPattern)
+#if QT_VERSION >= QT_VERSION_CHECK(5, 12, 0)
+        palette.setBrush(QPalette.Active, QPalette.PlaceholderText, brush5)
+#endif
+        palette.setBrush(QPalette.Inactive, QPalette.WindowText, brush)
+        palette.setBrush(QPalette.Inactive, QPalette.Button, brush1)
+        palette.setBrush(QPalette.Inactive, QPalette.Light, brush2)
+        palette.setBrush(QPalette.Inactive, QPalette.Midlight, brush1)
+        palette.setBrush(QPalette.Inactive, QPalette.Dark, brush1)
+        palette.setBrush(QPalette.Inactive, QPalette.Mid, brush1)
+        palette.setBrush(QPalette.Inactive, QPalette.Text, brush)
+        palette.setBrush(QPalette.Inactive, QPalette.ButtonText, brush)
+        palette.setBrush(QPalette.Inactive, QPalette.Base, brush3)
+        palette.setBrush(QPalette.Inactive, QPalette.Window, brush3)
+        palette.setBrush(QPalette.Inactive, QPalette.Highlight, brush4)
+        palette.setBrush(QPalette.Inactive, QPalette.ToolTipBase, brush2)
+#if QT_VERSION >= QT_VERSION_CHECK(5, 12, 0)
+        palette.setBrush(QPalette.Inactive, QPalette.PlaceholderText, brush5)
+#endif
+        palette.setBrush(QPalette.Disabled, QPalette.WindowText, brush1)
+        palette.setBrush(QPalette.Disabled, QPalette.Button, brush1)
+        palette.setBrush(QPalette.Disabled, QPalette.Light, brush2)
+        palette.setBrush(QPalette.Disabled, QPalette.Midlight, brush1)
+        palette.setBrush(QPalette.Disabled, QPalette.Dark, brush1)
+        palette.setBrush(QPalette.Disabled, QPalette.Mid, brush1)
+        palette.setBrush(QPalette.Disabled, QPalette.Text, brush1)
+        palette.setBrush(QPalette.Disabled, QPalette.ButtonText, brush1)
+        palette.setBrush(QPalette.Disabled, QPalette.Base, brush3)
+        palette.setBrush(QPalette.Disabled, QPalette.Window, brush3)
+        brush6 = QBrush(QColor(0, 120, 215, 255))
+        brush6.setStyle(Qt.SolidPattern)
+        palette.setBrush(QPalette.Disabled, QPalette.Highlight, brush6)
+        palette.setBrush(QPalette.Disabled, QPalette.ToolTipBase, brush2)
+#if QT_VERSION >= QT_VERSION_CHECK(5, 12, 0)
+        palette.setBrush(QPalette.Disabled, QPalette.PlaceholderText, brush5)
+#endif
+        MainWindow.setPalette(palette)
+        MainWindow.setMouseTracking(False)
+        MainWindow.setContextMenuPolicy(Qt.DefaultContextMenu)
+        MainWindow.setLayoutDirection(Qt.LeftToRight)
+        MainWindow.setAutoFillBackground(False)
+        MainWindow.setStyleSheet(u"QMainWindow{\n"
+"background: rgb(69, 60, 103)\n"
+"}")
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.pushButton = QPushButton(self.centralwidget)
+        self.pushButton.setObjectName(u"pushButton")
+        self.pushButton.setGeometry(QRect(300, 160, 171, 81))
+        self.pushButton.setStyleSheet(u"QPushButton{\n"
+"background-color : rgb(109, 103, 228)\n"
+"}")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+    # retranslateUi
+
